fb31ee58ed9a901cd80bc982c0b1b815261405a9
rakibul-wdp <rakibul.wdp@gmail.com>
2022-03-26 23:57:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
 import './App.css';
+import Faq from './components/Faq/Faq';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
+      <Faq></Faq>
     </div>
   );
 }
---
+++ src/components/Faq/Faq.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Faq.css';
+
+const Faq = () => {
+  return (
+    <div className='question'>
+      <h2 className='faq-header'>React Related FAQ...???</h2>
+      <div>
+        <h4>How React Works...???</h4>
+        <p>
+          React puts all of them in a root div. And create component base single page web application. React have JSX.It
+          look like html but not that. JSX is simply syntactic sugar for creating very specific JavaScript objects. In
+          react developer create multiple component and put them in app.js file like html tag. React mainly popular for
+          single page application. It's means it loaded one time hole website but not every time. If need any user
+          interacts declare state. One component to another component we will pass data by props.
+        </p>
+      </div>
+      <div>
+        <h4>Difference Between Props and State...!!!</h4>
+        <table>
+          <tr>
+            <th>Props</th>
+            <th>State</th>
+          </tr>
+          <tr>
+            <td>By props we pass data one component to another component</td>
+            <td>
+              When use interacts with website then state work. If user doesn't interacts with website state doesn't work
+            </td>
+          </tr>
+          <tr>
+            <td>It is Immutable (cannot be modified).</td>
+            <td>It is Mutable ( can be modified).</td>
+          </tr>
+          <tr>
+            <td>props need when pass data one component to other component</td>
+            <td>state need to functional activity</td>
+          </tr>
+          <tr>
+            <td>Props can be used with state and functional components.</td>
+            <td>components. State can be used only with the state components/class component</td>
+          </tr>
+          <tr>
+            <td>are used to pass data down from your view-controller</td>
+            <td>should not be accessed from child components</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
